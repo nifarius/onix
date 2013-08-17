997abf8edfea3ef780018bc940b7491380feeff4
Nifarius <nifarius@gmail.com>
2013-08-17 22:38:34 +0400

    Add IDTypeName to ProductIdentifier

--- lib/onix/product_identifier.rb
+++ lib/onix/product_identifier.rb
@@ -7,6 +7,7 @@ module ONIX
     xml_name "ProductIdentifier"
 
     xml_accessor :product_id_type, :from => "ProductIDType", :as => Fixnum, :to_xml => ONIX::Formatters.two_digit
+    xml_accessor :id_type_name, :from => "IDTypeName"
     xml_accessor :id_value, :from => "IDValue"
   end
 end
