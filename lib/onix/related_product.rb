50a3d3423e540e4b7af46abf2d5470747362f126
Nifarius <nifarius@gmail.com>
2014-04-25 13:58:02 +0400

    Parse relation_code as Fixnum

--- lib/onix/related_product.rb
+++ lib/onix/related_product.rb
@@ -4,7 +4,7 @@ module ONIX
 
     xml_name "RelatedProduct"
 
-    xml_accessor :relation_code,        from: "RelationCode",       to_xml: ONIX::Formatters.two_digit
+    xml_accessor :relation_code,        from: "RelationCode",       as: Fixnum, to_xml: ONIX::Formatters.two_digit
     xml_accessor :product_identifiers,  from: "ProductIdentifier",  as: [ONIX::ProductIdentifier]
 
     def initialize
