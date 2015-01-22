602359c757ad6a3fb4385856cdc6e4a4be19ba75
nifarius <nifarius@gmail.com>
2015-01-22 10:35:31 +0300

    Add cdata protection to Text tag.

--- lib/onix/other_text.rb
+++ lib/onix/other_text.rb
@@ -8,7 +8,7 @@ module ONIX
 
     xml_accessor :text_type_code, :from => "TextTypeCode", :as => Fixnum, :to_xml => ONIX::Formatters.two_digit
     xml_accessor :text_format,    :from => "TextFormat"
-    xml_accessor :text,           :from => "Text"
+    xml_accessor :text,           :from => "Text",         cdata: true
     xml_accessor :text_link_type, :from => "TextLinkType", :as => Fixnum, :to_xml => ONIX::Formatters.two_digit
     xml_accessor :text_link,      :from => "TextLink"
     xml_accessor :text_author,    :from => "TextAuthor"
